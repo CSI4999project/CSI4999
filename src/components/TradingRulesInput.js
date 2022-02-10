610d74d1914b687c978794351e4f5459b043f6e8
Rio Capollari <rcapollari@oakland.edu>
2022-02-10 18:01:32 -0500

    adds placeholders

--- src/components/TradingRulesInput.js
+++ src/components/TradingRulesInput.js
@@ -6,20 +6,22 @@ import Button from "@mui/material/Button";
 const TradingRulesInput = () => {
   return (
     <div>
-      <Card className="create">
+    <br />
+      <Card className="create"> {/* the styles are in app.css */}
         <CardContent>
           <h2>Trading Rules</h2>
+          <br />
           <form>
             <label>Starting Investmment</label>
-            <input type="text" required />
+            <input type="number" placeholder="Enter the amount you'd like to start investting" min="0" max="1000" step="5" required />
             <label>Party Size</label>
-            <input type="text" required />
+            <input type="text" placeholder="1-16" required />
             <label>Transaction Fees</label>
-            <input type="text" required />
+            <input type="text" placeholder="0-5%" required />
             <label>Time Frame</label>
-            <input type="text" required />
+            <input type="text" placeholder="Enter number of days" required />
             <label>Tokens Available</label>
-            <input type="text" required />
+            <input type="text" placeholder="0-300" required />
           </form>
           <Button variant="contained">Next</Button>
         </CardContent>
