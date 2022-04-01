cf968a72ac12ddff44fdc3b5e58e4f9ce6d3b85b
Rio Capollari <rcapollari@oakland.edu>
2022-04-01 17:41:11 -0400

    adds dm functionality

--- src/Pages/Chat/Chat.js
+++ src/Pages/Chat/Chat.js
@@ -1,14 +1,37 @@
-import { React, useContext } from "react";
-import { ChatEngine } from "react-chat-engine";
+import { React, useContext, useState } from "react";
+import { ChatEngine, getOrCreateChat } from "react-chat-engine";
 import ChatFeed from "../../components/ChatFeed";
 import "./ChatStyles.css";
-import { UserContext } from "../../context/userContext"
+import { UserContext } from "../../context/userContext";
 
 const projectID = process.env.REACT_APP_PROJECT_ID;
 
 const Chat = () => {
+  let { user, setUser } = useContext(UserContext);
+  const [username, setUsername] = useState("");
 
-  let {user, setUser} = useContext(UserContext);
+  function createDirectChat(creds) {
+    getOrCreateChat(
+      creds,
+      { is_direct_chat: true, usernames: [username] },
+      () => setUsername("")
+    );
+  }
+  function renderChatForm(creds) {
+    return (
+      <div>
+        <input
+          className="create-new-chat"
+          placeholder="Search User"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button className="dm-button" onClick={() => createDirectChat(creds)}>
+          Create Chat
+        </button>
+      </div>
+    );
+  }
 
   return (
     <ChatEngine
@@ -17,7 +40,12 @@ const Chat = () => {
       userName={user.username}
       userSecret={process.env.REACT_APP_CHAT_PASSWORD}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      renderNewChatForm={(creds) => renderChatForm(creds)}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
     />
   );
 };
